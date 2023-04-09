c9e93bcf837ea6a893d2ace44390ae8754a5c9c7
Gobaan <hi@gobaan.com>
2023-04-09 06:09:36 -0300

    remove stale configs, maybe move this to configuration in azure

--- lib/config.dart
+++ lib/config.dart
@@ -1,10 +1,8 @@
 class AppConfig {
   static const String apiUrl =
       'https://chatwithfriendsmvp.azurewebsites.net/api/';
-  static const String websiteUrl = 'http://localhost:63332/';
   static const String indexUrl = '${websiteUrl}#/';
   static const String negotiateUrl = '${apiUrl}negotiate';
-  static const String apiKey = 'your_api_key_here';
   static const int maxItemsPerPage = 20;
   static const languageCode = {
     'French': 'fr',
